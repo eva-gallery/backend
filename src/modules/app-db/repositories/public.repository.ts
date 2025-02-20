01eb127c19dbfe0d51ba2b38c9e2148eb6b5e606
Adam Lukacka <adam@lukacka.com>
2025-02-20 14:33:31 +0100

    Update public.repository.ts
    test include artwork.imageFilename, artwork.thumbnailFilename

--- src/modules/app-db/repositories/public.repository.ts
+++ src/modules/app-db/repositories/public.repository.ts
@@ -30,7 +30,7 @@ export class PublicRepository {
     return this.artists.manager.transaction(async mgr => {
       await mgr.query(`SELECT setseed(${nseed})`);
       const subQuery = mgr.getRepository(Artwork).createQueryBuilder("artwork")
-        .select(["artwork.id", "artwork.name", "artwork.label", "artwork.artist_id"])
+        .select(["artwork.id", "artwork.name", "artwork.label", "artwork.artist_id", "artwork.imageFilename", "artwork.thumbnailFilename"])
         .where("artwork.public = true")
         .andWhere("artwork.artist_id = artist.id")
         .orderBy("random()")
